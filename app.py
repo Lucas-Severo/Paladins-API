fb78cbaff8a560ce6334672b16ede60bf39bd902
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-02-15 09:10:08 -0400

    getResponse method implemented

--- app.py
+++ app.py
@@ -14,45 +14,44 @@ def getSession():
 def getSignature(method):
     return generateCredentials(devId, sessionId, method).generate_signature()
 
+def getResponse(method, session, signature, timestamp, player):
+    return requests.get(f"http://api.paladins.com/paladinsapi.svc/{method}json/\
+                            {devId}/{signature}/{session}/{timestamp}/{player}")
+
 @app.route("/getplayer/<player>")
 def getPlayer(player):
     session = getSession()
     signature, timestamp = getSignature("getplayer")
-    response = requests.get(f"http://api.paladins.com/paladinsapi.svc/getplayerjson/\
-                            {devId}/{signature}/{session}/{timestamp}/{player}")
+    response = getResponse("getplayer", session, signature, timestamp, player)
     return jsonify(response.json())
 
 @app.route("/getchampions/<player>")
 def getChampion(player):
     session = getSession()
     signature, timestamp = getSignature("getchampionranks")
-    response = requests.get(f"http://api.paladins.com/paladinsapi.svc/getchampionranksjson/\
-                            {devId}/{signature}/{session}/{timestamp}/{player}")
+    response = getResponse("getchampionranks", session, signature, timestamp, player)
     return jsonify(response.json())
 
 @app.route("/getstatus/<player>")
 def getStatus(player):
     session = getSession()
     signature, timestamp = getSignature("getplayerstatus")
-    response = requests.get(f"http://api.paladins.com/paladinsapi.svc/getplayerstatusjson/\
-                            {devId}/{signature}/{session}/{timestamp}/{player}")
+    response = getResponse("getplayerstatus", session, signature, timestamp, player)
     return jsonify(response.json())
 
-# returns all occurrences of a nickname (case insensitive)
+# returns all occurrences of a nickname
 @app.route("/searchplayers/<player>")
 def searchPlayer(player):
     session = getSession()
     signature, timestamp = getSignature("searchplayers")
-    response = requests.get(f"http://api.paladins.com/paladinsapi.svc/searchplayersjson/\
-                            {devId}/{signature}/{session}/{timestamp}/{player}")
+    response = getResponse("searchplayers", session, signature, timestamp, player)
     return jsonify(response.json())
 
 @app.route("/getmatches/<player>")
 def getMatches(player):
     session = getSession()
     signature, timestamp = getSignature("getmatchhistory")
-    response = requests.get(f"http://api.paladins.com/paladinsapi.svc/getmatchhistoryjson/\
-                            {devId}/{signature}/{session}/{timestamp}/{player}")
+    response = getResponse("getmatchhistory", session, signature, timestamp, player)
     return jsonify(response.json())
 
 if __name__ == "__main__":
